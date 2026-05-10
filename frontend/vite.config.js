daf30ef1fdfda8023872be909b2257c3de14b471
munibashami07 <munibashami07@gmail.com>
2026-05-09 17:48:47 -0700

    Landing page as entry point, app moved to app.html

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './app.html'
+      }
+    }
+  }
 })
